5daec8e1b55cf2b83033f7468a37824875e3c761
woongstar <qualtypo@gmail.com>
2025-09-27 11:24:59 +0900

    file을 위한 의존성 추가 및 정리 / 주석 추가

---
+++ src/main/java/com/clb/cliembing/config/UuidV7Generator.java
@@ -0,0 +1,13 @@
+package com.clb.cliembing.config;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class UuidV7Generator {
+    public UUID generate() {
+        return UuidCreator.getTimeOrderedEpoch();
+    }
+}
